202674ddb24526153fe4951d346101a14b2342d0
Alejandro Caicedo Cano <acaicedo@grupodecor.com>
2025-05-15 08:12:13 -0500

    edit Providers

---
+++ Pages/Products/Index/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SupermarketWEB.Pages.Products.Index
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Providers/Edit.cshtml.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Providers
+{
+    public class EditModel : PageModel
+    {
+       private readonly SupermarketContext _context;
+        public EditModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Provider Providers { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+            var providers = await _context.Providers.FirstOrDefaultAsync(m => m.Id == id );
+            if (providers == null)
+            {
+                return NotFound();
+            }
+             Providers = providers;
+             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.Attach(Providers).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProvidersExists(Providers.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToPage("./Index");
+        }
+        private bool ProvidersExists(int id)
+        {
+            return (_context.Providers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+
+
+
+    }
+}
